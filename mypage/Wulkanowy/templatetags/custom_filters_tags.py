7a067b9851a8502d73db68a7a3ae32af489153b5
Pengwius <tomekftomekf@gmail.com>
2020-12-11 09:12:28 +0100

    Transfer color customization from template

--- mypage/Wulkanowy/templatetags/custom_filters_tags.py
+++ mypage/Wulkanowy/templatetags/custom_filters_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -11,3 +12,20 @@ def return_item(l, i):
             return l[i]
     except:
         return None
+
+@register.filter
+def set_color(grade, value):
+    if value == '6' or value == '6-':
+        return mark_safe(f"<div class='grade' style='background-color: #3dbbf5;'>{grade}</div>")
+    elif value == '5' or value == '5-' or value == '5+':
+        return mark_safe(f"<div class='grade' style='background-color: #4caf50;'>{grade}</div>")
+    elif value == '4' or value == '4-' or value == '4+':
+        return mark_safe(f"<div class='grade' style='background-color: #a0c431;'>{grade}</div>")
+    elif value == '3' or value == '3-' or value == '3+':
+        return mark_safe(f"<div class='grade' style='background-color: #ffb940;'>{grade}</div>")
+    elif value == '2' or value == '2-' or value == '2+':
+        return mark_safe(f"<div class='grade' style='background-color: #ff774d;'>{grade }</div>")
+    elif value == '1' or value == '1+':
+        return mark_safe(f"<div class='grade' style='background-color: #d43f3f;'>{grade}</div>")
+    else:
+        return mark_safe(f"<div class='grade-else' style='background-color: #607d8b;'>{grade}</div>")
