b3f40570c9abc88439819f4054175d787859cda3
Pengwius <tomekftomekf@gmail.com>
2020-12-30 22:31:09 +0100

    Getting sent & deleted messages

--- mypage/Wulkanowy/API/messages.py
+++ mypage/Wulkanowy/API/messages.py
@@ -10,18 +10,55 @@ def get_messages(register_id, register_r, oun, s, date, school_year, symbol):
 
     now = calendar.timegm(time.gmtime())
     received_messages = []
+    sent_messages = []
+    deleted_messages = []
 
     messages = s.get('https://uonetplus-uzytkownik.vulcan.net.pl/nowysacz/Wiadomosc.mvc/GetInboxMessages?_dc='+str(now)+'&dataOd=&dataDo=&page=1&start=0&limit=25', headers=headers)
-
     for j in messages.json()['data']:
         received_messages.append({
             'Subject': j['Temat'],
             'Sender': j['Nadawca']['Name'],
             'Date': j['Data'],
-            'Read': j['Nieprzeczytana'],
+            'Read': j['Nieprzeczytana'] == False,
+            'Id': j['Id']
+        })
+
+    messages = s.get('https://uonetplus-uzytkownik.vulcan.net.pl/nowysacz/Wiadomosc.mvc/GetOutboxMessages?_dc='+str(now)+'&dataOd=&dataDo=&page=1&start=0&limit=25')
+    for j in messages.json()['data']:
+        sent_messages.append({
+            'Subject': j['Temat'],
+            'Recipients': get_recipients(j),
+            'Date': j['Data'],
+            'Read': j['Przeczytane'],
+            'Unread': j['Nieprzeczytane'],
+            'Id': j['Id']
+        })
+
+
+    messages = s.get('https://uonetplus-uzytkownik.vulcan.net.pl/nowysacz/Wiadomosc.mvc/GetTrashboxMessages?_dc='+str(now)+'&dataOd=&dataDo=&page=1&start=0&limit=25')
+    for j in messages.json()['data']:
+        deleted_messages.append({
+            'Subject': j['Temat'],
+            'Sender': j['Nadawca']['Name'],
+            'Date': j['Data'],
+            'Read': j['Nieprzeczytana'] == False,
             'Id': j['Id']
         })
 
     print(received_messages)
+    print('-------------------------------------------------------------------------')
+    print(sent_messages)
+    print('-------------------------------------------------------------------------')
+    print(deleted_messages)
+    print('-------------------------------------------------------------------------')
 
     return 'Bla'
+
+def get_recipients(j):
+    Recipients = []
+    for x in j['Adresaci']:
+        Recipients.append({
+            'Name': x['Name'],
+            'Read': x['Unreaded'] == False
+        })
+    return Recipients
