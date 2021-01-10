b6ce36d9d431ea3c8edabf9d6d43b19e50a80ec9
Pengwius <tomekftomekf@gmail.com>
2021-01-10 01:14:34 +0100

    Views test

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from app.views import default_view, content_view
-from app.views import login
+from app.views import login, grades, timetable, exams, homeworks, attendance, notes
 
 class TestUrls(SimpleTestCase):
     #views
@@ -17,3 +17,27 @@ class TestUrls(SimpleTestCase):
     def test_login_is_resolved(self):
         url = reverse('login')
         self.assertEquals(resolve(url).func, login)
+
+    def test_grades_is_resolved(self):
+        url = reverse('grades')
+        self.assertEquals(resolve(url).func, grades)
+
+    def test_timetable_is_resolved(self):
+        url = reverse('timetable')
+        self.assertEquals(resolve(url).func, timetable)
+
+    def test_exams_is_resolved(self):
+        url = reverse('exams')
+        self.assertEquals(resolve(url).func, exams)
+
+    def test_homeworks_is_resolved(self):
+        url = reverse('homeworks')
+        self.assertEquals(resolve(url).func, homeworks)
+
+    def test_attendance_is_resolved(self):
+        url = reverse('attendance')
+        self.assertEquals(resolve(url).func, attendance)
+
+    def test_notes_is_resolved(self):
+        url = reverse('notes')
+        self.assertEquals(resolve(url).func, notes)
