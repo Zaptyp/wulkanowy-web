bdeedecd4cb617ce6a0a70085c3a69521b0bc057
Pengwius <tomekftomekf@gmail.com>
2021-01-14 22:13:06 +0100

    Getting dashboard data

--- app/API/dashboard.py
+++ app/API/dashboard.py
@@ -31,8 +31,22 @@ def get_dashboard(register_id, register_r, s, diary_url, symbol):
     }
 
     index = requests.get(f'{diary_url}{symbol}/Start.mvc/Index', headers=headers, cookies=cookies)
-    permissionsValue = re.search("permissions: '(.)*'", index.text)
-    permissionsValue = permissionsValue.group()
-    print(permissionsValue)
+    permissions_value = re.search("permissions: '(.)*'", index.text)
+    permissions_value = permissions_value.group()
+    permissions_value = permissions_value.replace('permissions: ', '').replace("'", "")
 
-    return {"dupa": "dupa"}
+    json = {
+        "permissions": permissions_value
+    }
+
+    last_notes = requests.post(f'{diary_url}{symbol}/Start.mvc/GetLastNotes', headers=headers, cookies=cookies, json=json)
+    free_days = requests.post(f'{diary_url}{symbol}/Start.mvc/GetFreeDays', headers=headers, cookies=cookies, json=json)
+    lucky_number = requests.post(f'{diary_url}{symbol}/Start.mvc/GetKidsLuckyNumbers', headers=headers, cookies=cookies, json=json)
+    
+    return_data = {
+        "last_notes": last_notes.json(),
+        "free_days": free_days.json(),
+        "lucky_number": lucky_number.json()
+    }
+
+    return return_data
