3c6f2681a8eefdaa13ce35b9bf354ce8b69686eb
Tomasz <tomekftomekf@gmail.com>
2021-01-26 12:22:24 +0100

    Url's and login tests

--- frontend/static/frontend/js/account-manager.js
+++ frontend/static/frontend/js/account-manager.js
@@ -4,33 +4,33 @@ students = cookies_data.data.register_r.data;
 const displayData = () => {
     var primary = '';
 
-    students.forEach((student) =>{
-        if(student.UczenNazwisko+' '+student.UczenImie2+' '+student.UczenImie+' '+student.UczenSymbol != primary){
-            primary = student.UczenNazwisko+' '+student.UczenImie2+' '+student.UczenImie+' '+student.UczenSymbol;
+    students.forEach((student) => {
+        if (student.UczenNazwisko + ' ' + student.UczenImie2 + ' ' + student.UczenImie + ' ' + student.UczenSymbol != primary) {
+            primary = student.UczenNazwisko + ' ' + student.UczenImie2 + ' ' + student.UczenImie + ' ' + student.UczenSymbol;
             document.querySelector("#content").innerHTML += `<p>
             <label>
-            <input class="with-gap" name="group1" value='`+student.UczenPelnaNazwa+`' type="radio" checked />
-            <span>`+student.UczenPelnaNazwa+`</span>
+            <input class="with-gap" name="group1" value='`+ student.UczenPelnaNazwa + `' type="radio" checked />
+            <span>`+ student.UczenPelnaNazwa + `</span>
             </label>
-           </p>`
+           </p>`;
         }
     })
-    document.querySelector("#content").innerHTML += `<button id="button" class="waves-light waves-effect btn red darken-1">LOGIN</button>`
-    const button_ = document.querySelector("#button")
-    button_.addEventListener('click', logIn)
+    document.querySelector("#content").innerHTML += `<button id="button" class="waves-light waves-effect btn red darken-1">LOGIN</button>`;
+    const button_ = document.querySelector("#button");
+    button_.addEventListener('click', logIn);
 }
 
 const logIn = () => {
-    var ele = document.getElementsByName('group1'); 
-              
-    for(i = 0; i < ele.length; i++) { 
-        if(ele[i].checked){
+    var ele = document.getElementsByName('group1');
+
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
             studentName = ele[i].value;
             students.forEach((student) => {
-                if(student.UczenPelnaNazwa == studentName){
-                    cookies_data.data.register_r.data = [student]
+                if (student.UczenPelnaNazwa == studentName) {
+                    cookies_data.data.register_r.data = [student];
                     sessionStorage.setItem('cookies_data', JSON.stringify(cookies_data));
-                    window.location.href = '/content/'
+                    window.location.href = '/content/';
                 }
             })
         }
--- tests/test_views.py
+++ tests/test_views.py
@@ -5,6 +5,7 @@ import json
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
+        self.csrf_token = Client(enforce_csrf_checks=True)
         self.list_url = reverse('home')
         self.detail_url = reverse('content')
 
@@ -19,3 +20,24 @@ class TestViews(TestCase):
         response = self.client.get(self.detail_url)
 
         self.assertEquals(response.status_code, 302)
+
+        #ACCOUNT_MANAGER
+        response = self.client.get(reverse('account_manager'))
+        
+        self.assertEquals(response.status_code, 302)
+
+        #API
+        data = {
+            "loginName": "jan@fakelog.cf",
+            "Password": "jan123",
+            "Symbol": "powiatwulkanowy",
+            "diaryUrl": "http://cufs.fakelog.cf/"
+        }
+
+        response = self.client.post(reverse('login'), content_type='application/xml', data=json.dumps(data))
+        
+        self.assertEquals(response.status_code, 200)
+
+        #JAN
+
+        #JOANNA
