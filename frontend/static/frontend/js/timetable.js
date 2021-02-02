b546d511f4deb29f91af5b2b4f55f5857a2dd92d
Tomasz <tomekftomekf@gmail.com>
2021-02-02 10:53:10 +0100

    Fix buttons

--- frontend/static/frontend/js/timetable.js
+++ frontend/static/frontend/js/timetable.js
@@ -4,16 +4,13 @@ myStorage = window.sessionStorage;
 week = 0
 
 const getTimetable = (event) => {
-    if (event.target.id == 'previous') {
+    if (event.target.id == 'previous' || event.target.id == 'previous_i') {
         week -= 1 
     }
-    else if (event.target.id == 'next') {
+    else if (event.target.id == 'next' || event.target.id == 'next_i') {
         week += 1 
     }
-    else {
-        week = week
-    }
-    document.querySelector('#content').innerHTML = 'Here is timetable (in my imagination)<button id="previous" class="waves-light waves-effect btn red darken-1"><i class="material-icons">keyboard_arrow_left</i></button><button id="next" class="waves-light waves-effect btn red darken-1"><i class="material-icons">keyboard_arrow_right</i></button>';
+    document.querySelector('#content').innerHTML = 'Here is timetable (in my imagination)<button id="previous" class="waves-light waves-effect btn red darken-1"><i class="material-icons" id="previous_i" >keyboard_arrow_left</i></button><button id="next" class="waves-light waves-effect btn red darken-1"><i class="material-icons" id="next_i" >keyboard_arrow_right</i></button>';
     const left_ = document.querySelector('#previous');
     const right_ = document.querySelector('#next');
     left_.addEventListener('click', getTimetable);
