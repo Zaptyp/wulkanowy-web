1d2faef90e5b71d55a06c0c13b926e16327aad18
Tomasz <tomekftomekf@gmail.com>
2021-02-05 20:12:07 +0100

    Fix register_r cookie error

--- app/API/attendance.py
+++ app/API/attendance.py
@@ -1,22 +1,22 @@
 import requests
 import json
 
-def get_attendance(register_id, register_r, oun, s, date):
+def get_attendance(register_id, students, oun, s, date):
     cookies = s
     if oun != 'http://uonetplus-uczen.fakelog.cf/powiatwulkanowy/123458':
         cookies.update({
-            "biezacyRokSzkolny": f"{register_r['data'][0]['DziennikRokSzkolny']}",
-            "idBiezacyDziennik": f"{register_r['data'][0]['IdDziennik']}",
-            "idBiezacyDziennikPrzedszkole": f"{register_r['data'][0]['IdPrzedszkoleDziennik']}",
-            "idBiezacyDziennikWychowankowie": f"{register_r['data'][0]['IdWychowankowieDziennik']}",
-            "idBiezacyUczen": f"{register_r['data'][0]['IdUczen']}"
+            "biezacyRokSzkolny": f"{students['data'][0]['DziennikRokSzkolny']}",
+            "idBiezacyDziennik": f"{students['data'][0]['IdDziennik']}",
+            "idBiezacyDziennikPrzedszkole": f"{students['data'][0]['IdPrzedszkoleDziennik']}",
+            "idBiezacyDziennikWychowankowie": f"{students['data'][0]['IdWychowankowieDziennik']}",
+            "idBiezacyUczen": f"{students['data'][0]['IdUczen']}"
         })
     else:
         cookies.update({
-            "biezacyRokSzkolny": f"{register_r['data'][0]['DziennikRokSzkolny']}",
-            "idBiezacyDziennik": f"{register_r['data'][0]['IdDziennik']}",
-            "idBiezacyDziennikPrzedszkole": f"{register_r['data'][0]['IdPrzedszkoleDziennik']}",
-            "idBiezacyUczen": f"{register_r['data'][0]['IdUczen']}"
+            "biezacyRokSzkolny": f"{students['data'][0]['DziennikRokSzkolny']}",
+            "idBiezacyDziennik": f"{students['data'][0]['IdDziennik']}",
+            "idBiezacyDziennikPrzedszkole": f"{students['data'][0]['IdPrzedszkoleDziennik']}",
+            "idBiezacyUczen": f"{students['data'][0]['IdUczen']}"
         })
 
     headers = {
--- app/API/mobile_access.py
+++ app/API/mobile_access.py
@@ -1,22 +1,22 @@
 import json
 import requests
 
-def get_registered_devices(register_id, register_r, oun, s):
+def get_registered_devices(register_id, students, oun, s):
     cookies = s
     if oun != 'http://uonetplus-uczen.fakelog.cf/powiatwulkanowy/123458':
         cookies.update({
-            "biezacyRokSzkolny": f"{register_r['data'][0]['DziennikRokSzkolny']}",
-            "idBiezacyDziennik": f"{register_r['data'][0]['IdDziennik']}",
-            "idBiezacyDziennikPrzedszkole": f"{register_r['data'][0]['IdPrzedszkoleDziennik']}",
-            "idBiezacyDziennikWychowankowie": f"{register_r['data'][0]['IdWychowankowieDziennik']}",
-            "idBiezacyUczen": f"{register_r['data'][0]['IdUczen']}"
+            "biezacyRokSzkolny": f"{students['data'][0]['DziennikRokSzkolny']}",
+            "idBiezacyDziennik": f"{students['data'][0]['IdDziennik']}",
+            "idBiezacyDziennikPrzedszkole": f"{students['data'][0]['IdPrzedszkoleDziennik']}",
+            "idBiezacyDziennikWychowankowie": f"{students['data'][0]['IdWychowankowieDziennik']}",
+            "idBiezacyUczen": f"{students['data'][0]['IdUczen']}"
         })
     else:
         cookies.update({
-            "biezacyRokSzkolny": f"{register_r['data'][0]['DziennikRokSzkolny']}",
-            "idBiezacyDziennik": f"{register_r['data'][0]['IdDziennik']}",
-            "idBiezacyDziennikPrzedszkole": f"{register_r['data'][0]['IdPrzedszkoleDziennik']}",
-            "idBiezacyUczen": f"{register_r['data'][0]['IdUczen']}"
+            "biezacyRokSzkolny": f"{students['data'][0]['DziennikRokSzkolny']}",
+            "idBiezacyDziennik": f"{students['data'][0]['IdDziennik']}",
+            "idBiezacyDziennikPrzedszkole": f"{students['data'][0]['IdPrzedszkoleDziennik']}",
+            "idBiezacyUczen": f"{students['data'][0]['IdUczen']}"
         })
 
     headers = {
@@ -30,22 +30,22 @@ def get_registered_devices(register_id, register_r, oun, s):
 
     return registered.json()
 
-def register_device(register_id, register_r, oun, s):
+def register_device(register_id, students, oun, s):
     cookies = s
     if oun != 'http://uonetplus-uczen.fakelog.cf/powiatwulkanowy/123458':
         cookies.update({
-            "biezacyRokSzkolny": f"{register_r['data'][0]['DziennikRokSzkolny']}",
-            "idBiezacyDziennik": f"{register_r['data'][0]['IdDziennik']}",
-            "idBiezacyDziennikPrzedszkole": f"{register_r['data'][0]['IdPrzedszkoleDziennik']}",
-            "idBiezacyDziennikWychowankowie": f"{register_r['data'][0]['IdWychowankowieDziennik']}",
-            "idBiezacyUczen": f"{register_r['data'][0]['IdUczen']}"
+            "biezacyRokSzkolny": f"{students['data'][0]['DziennikRokSzkolny']}",
+            "idBiezacyDziennik": f"{students['data'][0]['IdDziennik']}",
+            "idBiezacyDziennikPrzedszkole": f"{students['data'][0]['IdPrzedszkoleDziennik']}",
+            "idBiezacyDziennikWychowankowie": f"{students['data'][0]['IdWychowankowieDziennik']}",
+            "idBiezacyUczen": f"{students['data'][0]['IdUczen']}"
         })
     else:
         cookies.update({
-            "biezacyRokSzkolny": f"{register_r['data'][0]['DziennikRokSzkolny']}",
-            "idBiezacyDziennik": f"{register_r['data'][0]['IdDziennik']}",
-            "idBiezacyDziennikPrzedszkole": f"{register_r['data'][0]['IdPrzedszkoleDziennik']}",
-            "idBiezacyUczen": f"{register_r['data'][0]['IdUczen']}"
+            "biezacyRokSzkolny": f"{students['data'][0]['DziennikRokSzkolny']}",
+            "idBiezacyDziennik": f"{students['data'][0]['IdDziennik']}",
+            "idBiezacyDziennikPrzedszkole": f"{students['data'][0]['IdPrzedszkoleDziennik']}",
+            "idBiezacyUczen": f"{students['data'][0]['IdUczen']}"
         })
 
     headers = {
