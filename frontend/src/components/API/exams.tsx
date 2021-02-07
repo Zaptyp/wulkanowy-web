f5592c8bfdb6132182ff5d521847a70c0a292e9b
Tomasz <tomekftomekf@gmail.com>
2021-02-07 21:31:37 +0100

    Getting data

--- frontend/src/components/API/exams.tsx
+++ frontend/src/components/API/exams.tsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class ExamsCom extends Component {
+    constructor(props) {
+        super(props);
+        var cookies_data = sessionStorage.getItem('cookies_data');
+        var csrfcookie_ = sessionStorage.getItem('csrfcookie');
+        fetch('../api/exams', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfcookie_
+            },
+            body: JSON.stringify({"cookies": cookies_data, "week": 0})
+        }).then(response => response.json()).then(data => {
+            console.log(data)
+        })
+        }
+
     render() {
         return (
             <p>Exams</p>
