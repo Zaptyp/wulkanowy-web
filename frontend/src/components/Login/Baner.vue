c33561968d3b5a306b60ed9aea4f769e98b32df7
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2021-09-05 14:50:18 +0200

    remove unused icon, change name of PP.vue, and remove char in wulkanowy.svg

--- frontend/src/components/Login/Baner.vue
+++ frontend/src/components/Login/Baner.vue
@@ -5,18 +5,18 @@
               <v-card dark color="#d32f2f" style="height: 524px; margin-right: 35px; width: 350px;">
                 <v-card-text>
                   <div style="width: 140; height: 140;">
-                    <img src="./iconTwo.svg"
+                    <img src="../../assets/wulkanowy.svg"
                     width="140"
                     class="logo"/>
                   </div>
-                <div id="hedlineRed">Wulkanowy</div>
-                <div id="hedlineTwo">Nieoficjalna aplikacja przeglądarkowa ucznia i
+                <div id="headlineRed">Wulkanowy</div>
+                <div id="headlineTwo">Nieoficjalna aplikacja przeglądarkowa ucznia i
                   rodzica dla dziennika Vulcan UONET+
                   </div>
                 <br><br><br>
                 <a href="#" target="_blank" style="color: white;">Polityka prywatności</a>
                 <br>
-                <div id="hedlineTwo">v 0.1.0</div>
+                <div id="headlineTwo">v 0.1.0</div>
                 </v-card-text>
               </v-card>
            </v-row>
@@ -31,7 +31,7 @@
   margin-bottom: 50px;
   width: 450px;
 }
-#hedlineRed{
+#headlineRed{
   text-align: center;
   font-weight: 300;
   font-size: 2pc;
@@ -42,7 +42,7 @@
   margin-top: 10px;
 }
 
-#hedlineTwo{
+#headlineTwo{
   text-align: center;
   font-weight: 300;
   font-size: 1pc;
