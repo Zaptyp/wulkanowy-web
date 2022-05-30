4332e2c44009ababda9691cab9734387da9ea691
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-05-30 08:15:30 +0200

    Add /uonetplus-uczen/school-info endpoint test!

---
+++ backend/tests/endpoints/school_info.py
@@ -0,0 +1,24 @@
+from tests.checks.status_code import status_check
+from tests.endpoints.login import client
+def school_info_test(cookies, headars, student, school_id, host, symbol, ssl, fg):
+    response = client.post(
+        "/uonetplus-uczen/school-info",
+        headers={"Content-Type": "application/json"},
+        json={
+            "vulcan_cookies": cookies,
+            "student": student,
+            "school_id": school_id,
+            "host": host,
+            "symbol": symbol,
+            "ssl": ssl,
+            "json": {},
+            "headers": headars,
+        },
+    )
+    status_check(response.status_code, response.json(), fg)
+    assert (
+        response.json()["school"]["name"]
+        == "Publiczna szkoła Wulkanowego nr 1 w fakelog.cf"
+    )
+    assert response.json()["teachers"][0]["name"] == "Karolina Kowalska [AN]"
+    return response.json()
