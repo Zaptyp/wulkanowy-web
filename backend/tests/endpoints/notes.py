0d92c9d0d476f4fcf8ede34f7f6c2c3ee1328e00
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-05-30 08:15:01 +0200

    Add /uonetplus-uczen/notes endpoint test!

---
+++ backend/tests/endpoints/notes.py
@@ -0,0 +1,21 @@
+from tests.checks.status_code import status_check
+from tests.endpoints.login import client
+def notes_test(cookies, headars, student, school_id, host, symbol, ssl, fg):
+    response = client.post(
+        "/uonetplus-uczen/notes",
+        headers={"Content-Type": "application/json"},
+        json={
+            "vulcan_cookies": cookies,
+            "student": student,
+            "school_id": school_id,
+            "host": host,
+            "symbol": symbol,
+            "ssl": ssl,
+            "json": {},
+            "headers": headars,
+        },
+    )
+    status_check(response.status_code, response.json(), fg)
+    assert response.json()["notes"][0]["teacher"] == "Karolina Kowalska [AN]"
+    assert response.json()["notes"][1]["content"] == "+ 20p za udział w Konkursie Języka Angielskiego"
+    #print(response.json())
