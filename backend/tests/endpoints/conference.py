fdecfe04e337140331df1b723841658fae4e7ed6
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-05-30 08:14:09 +0200

    Add /uonetplus-uczen/conferences endpoint test!

---
+++ backend/tests/endpoints/conference.py
@@ -0,0 +1,24 @@
+from tests.checks.status_code import status_check
+from tests.endpoints.login import client
+def conference_test(cookies, headars, student, school_id, host, symbol, ssl, fg):
+    response = client.post(
+        "/uonetplus-uczen/conferences",
+        headers={"Content-Type": "application/json"},
+        json={
+            "vulcan_cookies": cookies,
+            "student": student,
+            "school_id": school_id,
+            "host": host,
+            "symbol": symbol,
+            "ssl": ssl,
+            "json": {},
+            "headers": headars,
+        },
+    )
+    status_check(response.status_code, response.json(), fg)
+    assert (
+        response.json()[0]["subject"]
+        == "Podsumowanie I semestru - średnia klasy, oceny, frekwencja, zachowanie."
+    )
+    assert response.json()[1]["date"] == "06.09.2019 16:30"
+    # print(response.json())
