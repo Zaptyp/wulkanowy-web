97734766cd5bc9cd41f977a5acdc129df5f7a2be
Marioneq 4958 <mq4958@gmail.com>
2022-05-31 14:18:10 +0200

    Fix tests error part 1

--- backend/tests/endpoints/login.py
+++ backend/tests/endpoints/login.py
@@ -22,7 +22,7 @@ def login_test(nick, password, host, symbol, ssl, fg):
     student = login.json()["students"][0]["cookies"]
     school_id = login.json()["students"][0]["school_id"]
     status_check(login.status_code, login.json(), fg)
-    assert login.json()["symbol"] == "powiatwulkanowy"
+    assert login.json()["students"][0]["school_symbol"] == "powiatwulkanowy"
     assert login.json()["session_cookies"]
     try:
         assert login.json()["host"] == "fakelog.cf"
