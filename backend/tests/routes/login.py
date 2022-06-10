581df3416ecbd89eb70ae3e4149e3ce8cefeebd6
Marioneq 4958 <mq4958@gmail.com>
2022-06-10 15:28:26 +0200

    Fix tests part 8

--- backend/tests/routes/login.py
+++ backend/tests/routes/login.py
@@ -19,8 +19,9 @@ def login_test(nick, password, host, ssl, fg):
     session_data = login.json()[0]["session_data"]
     headers = login.json()[0]["schools"][0]["headers"]
     student = login.json()[0]["schools"][0]["students"][0]["cookies"]
+    symbol = login.json()[0]["name"]
     school_id = login.json()[0]["schools"][0]["id"]
     status_check(login.status_code, login.json(), fg)
     assert login.json()[0]["name"] == "powiatwulkanowy"
     assert login.json()[0]["session_data"]
-    return session_data, headers, student, school_id
+    return session_data, headers, student, school_id, symbol
