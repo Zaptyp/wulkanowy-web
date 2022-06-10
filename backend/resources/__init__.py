ff2bcdbf3a85d2227883f3b4f9dd951569606995
Marioneq 4958 <mq4958@gmail.com>
2022-06-10 09:43:47 +0200

    Fix errors part 1

--- backend/resources/__init__.py
+++ backend/resources/__init__.py
@@ -1,5 +1,1 @@
-INCORRECT_CREDENTIALS = "incorrect_username_or_password"
-INVALID_HOST = "invalid_host"
-NO_SYMBOLS = "no_students_account"
-AUTHENTICATION_REQUIRED = "authentication_required"
-UNKNOW_ERROR = "unknow_error"
+from .errors import *
